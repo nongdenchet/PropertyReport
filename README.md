6ffe51564280501fa8b3235e647d6ad05134b02b
Quan Vu <nongdenchet8435848@gmail.com>
2020-02-09 17:28:20 +1100

    [Config] Initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+NSW Property Report
---
+++ src/test/java/rain/property/report/PropertyReportApplicationTests.java
@@ -0,0 +1,13 @@
+package rain.property.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
